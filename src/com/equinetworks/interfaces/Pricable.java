303377829a2798ab981c4e58eb13aadbd5ea9507
Ruejee <dbyx.project@gmail.com>
2020-04-01 14:34:39 +0300

    Code optimisation, Learning Java Interfaces Part II, on real example "Supermarket Delivering System".

---
+++ src/com/equinetworks/interfaces/Pricable.java
@@ -0,0 +1,9 @@
+package com.equinetworks.interfaces;
+
+public interface Pricable extends Deliverable,Orderable {
+
+    default  int calcPrice(){
+        return calcOrderPrice()+calcDeliveryPrice();
+    }
+
+}
