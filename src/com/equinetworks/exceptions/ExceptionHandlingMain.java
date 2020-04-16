1bbdb64febf412d096ee301f6692cfa21a613ae1
Cristian <35853294+ruejee@users.noreply.github.com>
2020-04-16 04:45:24 +0300

    Learning Java Exception, optimising code use force exception to throw IOException, learning basics of make external file like out.txt, and understanding concept of Exception.

--- src/com/equinetworks/exceptions/ExceptionHandlingMain.java
+++ src/com/equinetworks/exceptions/ExceptionHandlingMain.java
@@ -1,5 +1,8 @@
 package com.equinetworks.exceptions;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -7,18 +10,30 @@ public class ExceptionHandlingMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean continueLoop = true;
+        PrintWriter writer = null;
         do {
             try {
                 System.out.println("Please enter numerator ");
                 int numerator = scanner.nextInt();
                 System.out.println("Please enter denominator");
                 int denominator = scanner.nextInt();
-                System.out.println(divide(numerator, denominator));
+//                System.out.println(divide(numerator, denominator));
+                writer = new PrintWriter(new FileWriter("out.txt"));
+                writer.println("Result = " + divide(numerator, denominator));
+                if (continueLoop) {
+                    throw new RuntimeException("Runtime exception");
+                }
                 continueLoop = false;
             } catch (ArithmeticException | InputMismatchException e) {
                 System.out.println("Exception : " + e);
                 scanner.nextLine();
                 System.out.println("Only integer non-zero parameters allowed");
+            } catch (IOException e) {
+                System.out.println("Unable to open file");
+                e.printStackTrace();
+            } finally {
+                System.out.println("Finally block was called");
+                writer.close();
             }
             System.out.println("Try Catch block finished");
         } while (continueLoop);
@@ -27,5 +42,12 @@ public class ExceptionHandlingMain {
     private static int divide(int numerator, int denominator) {
         return numerator / denominator;
     }
+
+    /*
+    private static void saveToFile(int res) throws IOException {
+        PrintWriter writer = new PrintWriter(new FileWriter("out.txt"));
+        writer.println("Result = " + res);
+        writer.close();
+    }*/
 
 }
